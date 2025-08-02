38da3be4f24ac7e3d0af52c5a1a48fae40a17ca7
DaSH <125895664+Dimonlomon@users.noreply.github.com>
2025-08-03 00:17:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ from PIL import ImageGrab
 import pyautogui
 
 # Настройки сервера
-HOST = '0.0.0.0'     # IP-адрес сервера
+HOST = 'localhost'     # IP-адрес сервера
 PORT = 5000               # Порт
 PASSWORD = 'StrongPassword123'
 
@@ -157,3 +157,4 @@ def start_server():
 
 if __name__ == '__main__':
     start_server()
+
